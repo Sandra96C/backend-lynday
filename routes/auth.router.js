e954e767e3ef79eaa3802ee3c5f949630ab0f3a5
Sandra Colomer <3496sandra@gmail.com>
2026-05-29 18:13:52 +0200

    create middleware to register

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", authMiddleware, register);
 router.post("/login", login);
 
 export default router;
