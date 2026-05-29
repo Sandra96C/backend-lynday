0173b976f0f4dae1485880ee1294befdced12149
Sandra Colomer <3496sandra@gmail.com>
2026-05-29 15:15:54 +0200

    create authRouter and authController

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import "./db.js";
+import authRouter from "./routes/auth.router.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ error: "Not found" });
