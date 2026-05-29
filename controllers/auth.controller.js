134e79a7392a69d31f061572781fde0e7d2c3191
Sandra Colomer <3496sandra@gmail.com>
2026-05-29 17:54:16 +0200

    login and register fn controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,103 @@
-export const register = (req, res) => {
-  res.send("Register endpoint");
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (user) => {
+  return jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 };
 
-export const login = (req, res) => {
-  res.send("Login endpoint");
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const normalizedEmail = email?.trim().toLowerCase();
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        error: "Complete the fields please",
+      });
+    }
+
+    const existingUser = await User.findOne({ email: normalizedEmail });
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name: name.trim().toLowerCase(),
+      email: normalizedEmail,
+      password: hashedPassword,
+      role: role || "user",
+    });
+
+    res.status(201).json({
+      id: user._id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error creating user" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const normalizedEmail = email?.trim().toLowerCase();
+
+    if (!normalizedEmail || !password) {
+      return res.status(400).json({
+        error: "Email and password required",
+      });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(normalizedEmail)) {
+      return res.status(400).json({
+        error: "Invalid email",
+      });
+    }
+
+    const user = await User.findOne({ email: normalizedEmail });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Invalid Credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        error: "Invalid Credentials",
+      });
+    }
+
+    const token = createToken(user);
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      error: "Error logging in",
+    });
+  }
 };
