1050bea3818327d24416c4e8153497da6fb71e21
Sandra Colomer <3496sandra@gmail.com>
2026-06-12 14:43:50 +0200

    create get profile auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -112,3 +112,21 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const profile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: "Error getting profile",
+    });
+  }
+};
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, profile } from "../controllers/auth.controller.js";
 import { authMiddleware, isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", authMiddleware, isAdmin, register);
 router.post("/login", login);
+
+router.get("/profile", authMiddleware, profile);
 
 export default router;
